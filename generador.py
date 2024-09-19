7ff47989e4be9e1c31533a425847db90306e60e2
SantiLP <34819955+SantiagoLucia@users.noreply.github.com>
2024-09-19 11:03:46 -0300

    refactorizacion de codigo

--- generador.py
+++ generador.py
@@ -1,138 +1,131 @@
 import pandas as pd
-from tabulate import tabulate
 import sqlalchemy
 from config import CON_URL
-import strings
+import templates
 
 
-def escribir_archivo(nom_archivo, seccion, modo, *args):
+def generar_seccion(seccion: str, *args) -> str:
+    """Genera una sección de un script SQL a partir de un template y argumentos."""
     secciones = {
-        "encabezado": strings.encabezado,
-        "final": strings.final,
-        "insert": strings.insert,
-        "encabezado_kodak": strings.encabezado_kodak,
-        "final_kodak": strings.final,
-        "insert_kodak": strings.insert_kodak,
+        "encabezado": templates.encabezado,
+        "final": templates.final,
+        "insert": templates.insert,
+        "encabezado_kodak": templates.encabezado_kodak,
+        "final_kodak": templates.final,
+        "insert_kodak": templates.insert_kodak,
     }
 
     template = secciones.get(seccion)
-    identificadores = template.get_identifiers()
-    mapeo = {identificador: args[i] for i, identificador in enumerate(identificadores)}
+    campos = template.get_identifiers()
+    valores = args
+    mapeo = {campo: valores[i] for i, campo in enumerate(campos)}
 
+    return template.substitute(**mapeo)
+
+
+def escribir_archivo(nom_archivo: str, contenido: str, modo: str) -> None:
+    """Escribe un archivo con el contenido indicado."""
     with open(f"./data/{nom_archivo}", modo) as file:
-        file.write(template.substitute(**mapeo))
+        file.write(contenido)
 
 
-def crear_scripts_sql(df):
-    flag_encabezado_kodak = 1
-    cant1 = 0
-    cant2 = 0
-    nombre_archivo_d = "PROD - 1 - INSERT DELEGACIONES.sql"
+def obtener_ultimo_id(connetion, query) -> int:
+    """Obtiene el último ID de la base de datos."""
+    try:
+        data = pd.read_sql(query, connetion)
+        id = data.loc[0]["id"]
+        return id
+    except:
+        raise Exception("Error al obtener el último ID en la consulta: ", query)
+
+
+def obtener_cantidad_inserts(df: pd.DataFrame) -> dict:
+    """Obtiene la cantidad de inserts a realizar."""
+    cant_inserts_delegaciones = 0
+    cant_inserts_kodak = 0
+
+    for _, row in df.iterrows():
+        if row["TIPO_REGISTRO"] in (3, 5, 6):
+            cant_inserts_kodak += 1
+        cant_inserts_delegaciones += 1
+
+    return {"delegaciones": cant_inserts_delegaciones, "kodak": cant_inserts_kodak}
+
+
+def main():
+    df = pd.read_excel("./data/delegaciones.xlsx")
+    archivo_delegaciones = "PROD - 1 - INSERT DELEGACIONES.sql"
+    archivo_kodak = "PROD - 2 - INSERT DELEGACIONES KODAK.sql"
+    escribir_kodak = False
 
     engine = sqlalchemy.create_engine(CON_URL)
+
+    cantidad_inserts = obtener_cantidad_inserts(df)
+
+    if (cantidad_inserts["delegaciones"] == 0):
+        return
+
     with engine.connect() as conn:
-        print("Obteniendo último ID de delegación...")
+        
         query = sqlalchemy.text(
-            """SELECT ID_CIRCUNSCRIPCION FROM 
+            """SELECT ID_CIRCUNSCRIPCION AS ID FROM 
             RCE_GED.SYS_CIRCUNSCRIPCION ORDER BY ID_CIRCUNSCRIPCION DESC
             fetch first 1 rows only"""
         )
-        data = pd.read_sql(query, conn)
-        # id_cir = data.iloc[0][0]
-        id_cir = data.loc[0]["id_circunscripcion"]
-
-        print("Obteniendo último ID KODAK...")
-        query = sqlalchemy.text(
-            """SELECT ID_KODAK_CIRCUNSCRIPCION FROM RCE_GED.SYS_KODAK_CIRCUNSCRIPCION 
-            ORDER BY ID_KODAK_CIRCUNSCRIPCION DESC
-            fetch first 1 rows only"""
-        )
-        data = pd.read_sql(query, conn)
-
-        k_ini = data.loc[0]["id_kodak_circunscripcion"]
-        k_ini += 1
-        k_ini = str(k_ini)
-        print("OK")
-
-        for _, row in df.iterrows():
-            if row["TIPO REGISTRO"] in (3, 5, 6):
-                cant2 += 1
-            cant1 += 1
-
-        escribir_archivo(nombre_archivo_d, "encabezado", "w+", cant1)
-
-        for _, row in df.iterrows():
-            id_cir += 1
-            id_cir_str = str(id_cir)
-
-            nom = row["NOMBRE DELEGACION"]
-            nom = str(nom).strip()
-
-            reg = row["TIPO REGISTRO"]
-            reg = str(reg).strip()
-
-            renaper = row["COD RENAPER"]
-            renaper = str(renaper).strip()
-
-            nom_loc = row["LOCALIDAD"]
-            nom_loc = str(nom_loc).strip()
-            nom_loc = nom_loc.upper()
-
-            nom_dep = row["PARTIDO"]
-            nom_dep = str(nom_dep).strip()
-            nom_dep = nom_dep.upper()
-
-            try:
-                print(f"Obteniendo ID de localidad {nom_loc}...")
-                query = sqlalchemy.text(
-                    f"""SELECT LOC.ID
-                    FROM EU_GED.EU_LOCALIDAD  LOC
-                    INNER JOIN EU_GED.EU_DEPARTAMENTO DEP ON LOC.ID_DEPARTAMENTO = DEP.ID
-                    WHERE LOC.NOMBRE = '{nom_loc}' AND DEP.NOMBRE = '{nom_dep}' AND LOC.ID_PROVINCIA = 97
-                    """
-                )
-                data = pd.read_sql(query, conn)
-                res = data.loc[0]["id"]
-                loc = str(res)
-
-            except:
-                print("Error al obtener ID")
-                loc = "xxxxx"
-
-            escribir_archivo(
-                nombre_archivo_d,
-                "insert",
-                "a",
-                id_cir_str,
-                id_cir_str,
-                nom,
-                reg,
-                renaper,
-                loc,
+        id_delegacion = obtener_ultimo_id(conn, query)
+        contenido = generar_seccion("encabezado", cantidad_inserts["delegaciones"])
+        escribir_archivo(archivo_delegaciones, contenido, "w+")
+        
+        if (cantidad_inserts["kodak"] > 0):
+            escribir_kodak = True
+            query = sqlalchemy.text(
+                """SELECT ID_KODAK_CIRCUNSCRIPCION AS ID FROM RCE_GED.SYS_KODAK_CIRCUNSCRIPCION 
+                ORDER BY ID_KODAK_CIRCUNSCRIPCION DESC
+                fetch first 1 rows only"""
             )
+            id_kodak = obtener_ultimo_id(conn, query)
+            contenido = generar_seccion("encabezado_kodak", cantidad_inserts["kodak"], id_kodak)
+            escribir_archivo(archivo_kodak, contenido, "w+")
 
-            if int(reg) in (3, 5, 6):
-                if flag_encabezado_kodak == 1:
-                    flag_encabezado_kodak = 0
-                    nombre_archivo_k = "PROD - 2 - INSERT DELEGACIONES KODAK.sql"
-                    escribir_archivo(
-                        nombre_archivo_k, "encabezado_kodak", "w+", cant2, k_ini
-                    )
+        for _, row in df.iterrows():
+            id_delegacion += 1
+            delegacion = str(row["DELEGACION"]).strip()
+            tipo_registro = str(row["TIPO_REGISTRO"]).strip()
+            codigo_renaper = str(row["CODIGO_RENAPER"]).strip()
+            localidad = str(row["LOCALIDAD"]).strip().upper()
+            departamento = str(row["DEPARTAMENTO"]).strip().upper()
+            
+            query = sqlalchemy.text(
+                f"""SELECT LOC.ID
+                FROM EU_GED.EU_LOCALIDAD  LOC
+                INNER JOIN EU_GED.EU_DEPARTAMENTO DEP ON LOC.ID_DEPARTAMENTO = DEP.ID
+                WHERE LOC.NOMBRE = '{localidad}' AND DEP.NOMBRE = '{departamento}' AND LOC.ID_PROVINCIA = 97
+                """
+            )
+            id_localidad = obtener_ultimo_id(conn, query)
+            
+            contenido = generar_seccion(
+                "insert",
+                id_delegacion,
+                id_delegacion,
+                delegacion,
+                tipo_registro,
+                codigo_renaper,
+                id_localidad,
+            )
+            escribir_archivo(archivo_delegaciones, contenido, "a")
 
-                escribir_archivo(
-                    nombre_archivo_k, "insert_kodak", "a", reg, renaper, id_cir_str
-                )
+            if int(tipo_registro) in (3, 5, 6):
+                contenido = generar_seccion("insert_kodak", tipo_registro, codigo_renaper, id_delegacion)
+                escribir_archivo(archivo_kodak, contenido, "a")
 
-        escribir_archivo(nombre_archivo_d, "final", "a")
-        print(f"{nombre_archivo_d} generado correctamente")
 
-        if flag_encabezado_kodak == 0:
-            escribir_archivo(nombre_archivo_k, "final_kodak", "a")
-            print(f"{nombre_archivo_k} generado correctamente")
+        contenido = generar_seccion("final")
+        escribir_archivo(archivo_delegaciones, contenido, "a")
+        if escribir_kodak:
+            contenido = generar_seccion("final_kodak")
+            escribir_archivo(archivo_kodak, contenido, "a")
 
 
 if __name__ == "__main__":
-    df = pd.read_excel("./data/delegaciones.xlsx")
-    pdtabulate = lambda df: tabulate(df, headers="keys", tablefmt="psql")
-    print(pdtabulate(df))
-    crear_scripts_sql(df)
+    main()
